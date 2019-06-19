77022dc1bfb10b36b68bb0da249f24b48cfa3b0d
Aashna <bajwa.aashna@gmail.com>
2019-06-19 11:26:03 -0400

    Part 1 & 2 complete

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,27 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+@Test
+    public void testConstructor(){
+    Person person = new Person(456, "Aashna");
+
+    long expected = 456;
+    long actual = person.getId();
+
+    Assert.assertEquals(expected, actual);
+}
+
+@Test
+    public void testSetName(){
+    Person person = new Person(456, "Aashna");
+
+    String expected = "Aashna";
+    String actual = person.getName();
+
+    Assert.assertEquals(expected, actual);
+}
 }
